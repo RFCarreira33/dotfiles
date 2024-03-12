8860a5dd1e9e0f96143cd185ec9d57c1b7df56cf
Rodrigo Carreira <rodrigocarreira33@gmail.com>
2024-03-12 16:43:28 +0000

    Mardown Preview

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -50,5 +50,15 @@ return {
     },
   },
 
-  {'akinsho/git-conflict.nvim', version = "*", config = true},
+  { 'akinsho/git-conflict.nvim', version = "*", config = true },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }

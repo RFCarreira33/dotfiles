f6af4f23931fbfbbef6d5ea352e67d06036093e7
Rodrigo Carreira <rodrigocarreira33@gmail.com>
2026-03-16 21:48:39 +0000

    tex config update

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -86,7 +86,15 @@ return {
       local g = vim.g
       g.vimtex_quickfix_mode = 0
       g.vimtex_view_method = "zathura"
-      g.vimtex_view_general_viewer = "zathura"
+      g.vimtex_compiler_latexmk = {
+        executable = "latexmk",
+        options = {
+          "-pdf",
+          "-xelatex",
+          "-synctex=1",
+          "-interaction=nonstopmode",
+        },
+      }
     end,
   },
 
